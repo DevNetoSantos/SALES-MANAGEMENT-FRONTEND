221602ba5ffda3c6cf62562cd9b25752a9b7c9a4
Raimundo Neto <rasneto147@gmail.com>
2022-11-30 08:44:53 -0300

    added bootstrap

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './App'
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from './app/context/AuthProvider';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <AuthProvider>
