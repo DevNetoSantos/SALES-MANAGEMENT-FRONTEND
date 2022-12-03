0e9924a6c7b74e933b46d4ea965aa9ed05f2db70
Raimundo Neto <rasneto147@gmail.com>
2022-12-02 22:58:26 -0300

    modal details reday

--- src/app/pages/Employee/index.tsx
+++ src/app/pages/Employee/index.tsx
@@ -5,6 +5,16 @@ import { TypeEmployee } from "../../Types/TypesEmployee";
 
 export const Employee = () => {
   const [employee, setEmployee] = useState<TypeEmployee[]>([]);
+  const [item,setItem] = useState({
+    id: '',
+    createdAt: '',
+    updatedAt: '',
+    name: '',
+    lastname: '',
+    email: '',
+    password: '',
+    role: '',
+ })
   const auth = useAuth()
 
   const getEmployee = async () => {
@@ -25,6 +35,16 @@ export const Employee = () => {
     getEmployee()
   }, []);
 
+  const showDetail = async (id: number) =>
+  {
+    const response = await api.get(`employee/${id}`, {
+      headers: {
+        Authorization: `Bearer ${auth.access_token}`
+      }
+    })
+    setItem(response.data)
+  }
+
   return(
     <div className="">
         <table className="table">
@@ -43,12 +63,38 @@ export const Employee = () => {
                 <td>{item.lastname}</td>
                 <td>{item.email}</td>
                 <td>
-                  <button className="btn btn-primary">View</button>
+                  <button type="button" className="btn btn-primary"  onClick={(e)=>showDetail(item.id)} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    View
+                  </button>
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
+
+        <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div className="modal-dialog">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div className="modal-body">
+              <p>Data Criação : {item.createdAt}</p>
+              <p>Data Atualização : {item.updatedAt}</p>
+              <p>Name : {item.name}</p>
+              <p>Sobrenome : {item.lastname}</p>
+              <p>Email : {item.email}</p>
+              <p>conta : {item.role}</p>
+
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
