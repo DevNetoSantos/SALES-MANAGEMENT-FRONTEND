636b3a10fe00d5351e4f73920e1e459d042243c0
Raimundo Neto <rasneto147@gmail.com>
2022-12-05 18:56:31 -0300

    view client ready

--- src/app/pages/Client/index.tsx
+++ src/app/pages/Client/index.tsx
@@ -1,13 +1,31 @@
 import { useEffect, useState } from 'react';
 import styles from '../Client/Client.module.css';
 import { TypeClient } from '../../Types/TypesClient';
-import axios from 'axios';
 import { api } from '../../services/api';
 import { useAuth } from '../../context/AuthProvider/useAuth';
+import { Pagination } from '../../components/Pagination';
 
 export const Client = () => {
   const [client, setClient] = useState<TypeClient[]>([]);
+  const [takePage, setTakePage] = useState(1); //mudar depois para 5
+  const [skipPage, setSkipPage] = useState(1);
+  const [item,setItem] = useState({
+    id: '',
+    createdAt: '',
+    updatedAt: '',
+    name: '',
+    cpf: ''
+ })
   const auth = useAuth()
+
+  const startIndex = skipPage * takePage;
+  const endIndex = startIndex - takePage;
+  const currentClient = client.slice(endIndex, startIndex);
+
+  const paginate = (pageNumber: any) => {
+    setSkipPage(pageNumber);
+  }
+
 
   useEffect(() =>{
     const fetchCliet = async () => {
@@ -25,8 +43,18 @@ export const Client = () => {
     fetchCliet();
   }, []);
 
+  const showDetail = async (id: number) =>
+  {
+    const response = await api.get(`client/${id}`, {
+      headers: {
+        Authorization: `Bearer ${auth.access_token}`
+      }
+    })
+    setItem(response.data)
+  }
+
   return(
-    <div className="">
+    <div className={styles.container}>
       <table className="table">
         <thead>
           <tr>
@@ -36,15 +64,43 @@ export const Client = () => {
           </tr>
         </thead>
         <tbody>
-          {client.map((item, index) =>(
+          {currentClient.map((item, index) =>(
             <tr key={index}>
               <td>{item.name}</td>
               <td>{item.cpf}</td>
-              <td>@mdo</td>
+              <td>
+                <button type="button" className="btn btn-primary"  onClick={(e)=>showDetail(item.id)} data-bs-toggle="modal" data-bs-target="#exampleModal">
+                  View
+                </button>
+              </td>
             </tr>  
           ))}
         </tbody>
       </table>
+
+      <Pagination takePage={takePage} totalNames={client.length} paginate={paginate}/>
+
+      <div className="modal fade" id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">Detalhes</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+            <h6>Data Criação : {item.createdAt.slice(0,10)}</h6>
+            <h6>Data Atualização : {item.updatedAt.slice(0,10)}</h6>
+            <h6>Name : {item.name}</h6>
+            <h6>Cpf : {item.cpf}</h6>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button type="button" className="btn btn-primary">Editar</button>
+              <button type="button" className="btn btn-danger">Deletar</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>  
   )
 }
